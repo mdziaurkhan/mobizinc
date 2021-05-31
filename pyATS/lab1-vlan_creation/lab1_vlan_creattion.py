9f76a8f69db21a21f5d1670c56f080f02fb194f6
mdziaurkhan <mdziaurkhan@gmail.com>
2021-05-31 16:59:08 -0500

    updating loop

--- pyATS/lab1-vlan_creation/lab1_vlan_creattion.py
+++ pyATS/lab1-vlan_creation/lab1_vlan_creattion.py
@@ -5,6 +5,6 @@ testbed = loader.load('lab1_testbed_con.yaml')
 testbed.devices
 cisco_switches = testbed.devices
 for switch in cisco_switches:
-    cisco_switches.connect()
-    print (cisco_switches.execute('show interface status'))
-    cisco_switches.disconnect()
+    switch.connect()
+    print (switch.execute('show interface status'))
+    switch.disconnect()
