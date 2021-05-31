2f157a35bb28a358d9e5c32e7fbae75d7621832e
mdziaurkhan <mdziaurkhan@gmail.com>
2021-05-31 17:35:57 -0500

    going back to working state

--- pyATS/lab1-vlan_creation/lab1_vlan_creattion.py
+++ pyATS/lab1-vlan_creation/lab1_vlan_creattion.py
@@ -3,9 +3,9 @@ from pyats.topology import loader
 testbed = loader.load('lab1_testbed_con.yaml')
 
 testbed.devices
+cisco_switches = testbed.devices['SW-192.168.1.11']
 
-for switch in cisco_switches:
-    cisco_switches = testbed.devices
-    switch.connect()
-    print (switch.execute('show interface status'))
-    switch.disconnect()
+cisco_switches.connect ()
+
+print (cisco_switches.execute('show version'))
+cisco_switches.disconnect ()
