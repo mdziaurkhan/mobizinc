1ed6deb3fd3e2fda44694a6d55b80e2e93c134cf
mdziaurkhan <mdziaurkhan@gmail.com>
2021-05-30 23:42:37 -0500

    first script

---
+++ pyATS/lab1-vlan_creation/lab1_vlan_creattion.py
@@ -0,0 +1,12 @@
+from pyats.topology import loader
+
+testbed = loader.load('lab1-testbed.yml')
+
+testbed.devices
+cisco_switches = testbed.devices['swithces']
+
+cisco_switches.connect ()
+
+print (cisco_switches.execute('show version'))
+
+cisco_switches.disconnect ()
