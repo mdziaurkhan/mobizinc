67c006deca187ec9b44cfe3c9b1b213e981ae7ce
mdziaurkhan <mdziaurkhan@gmail.com>
2021-05-31 17:01:57 -0500

    checking

--- pyATS/lab1-vlan_creation/lab1_vlan_creattion.py
+++ pyATS/lab1-vlan_creation/lab1_vlan_creattion.py
@@ -3,8 +3,9 @@ from pyats.topology import loader
 testbed = loader.load('lab1_testbed_con.yaml')
 
 testbed.devices
-cisco_switches = testbed.devices
+
 for switch in cisco_switches:
+    cisco_switches = testbed.devices
     switch.connect()
     print (switch.execute('show interface status'))
     switch.disconnect()
