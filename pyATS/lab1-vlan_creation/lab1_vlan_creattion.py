c5dd2f92e98793b7967cecf651a84a1617483ae5
mdziaurkhan <mdziaurkhan@gmail.com>
2021-05-31 16:05:02 -0500

    update cisco_switches

--- pyATS/lab1-vlan_creation/lab1_vlan_creattion.py
+++ pyATS/lab1-vlan_creation/lab1_vlan_creattion.py
@@ -3,7 +3,7 @@ from pyats.topology import loader
 testbed = loader.load('lab1_testbed_con.yaml')
 
 testbed.devices
-cisco_switches = testbed.devices['SW-192.168.1.11']
+cisco_switches = testbed.devices
 
 cisco_switches.connect ()
 
