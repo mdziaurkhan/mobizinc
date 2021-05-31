cba3cea88b7b2ecdc05106d6d59f4c6d737ff9a4
mdziaurkhan <mdziaurkhan@gmail.com>
2021-05-31 16:37:11 -0500

    correcting for loop indentation

--- pyATS/lab1-vlan_creation/lab1_vlan_creattion.py
+++ pyATS/lab1-vlan_creation/lab1_vlan_creattion.py
@@ -3,10 +3,7 @@ from pyats.topology import loader
 testbed = loader.load('lab1_testbed_con.yaml')
 
 testbed.devices
-for cisco_switches in testbed.devices
-
-cisco_switches.connect ()
-
-print (cisco_switches.execute('show interface status'))
-
-cisco_switches.disconnect ()
+for cisco_switches in testbed.devices:
+    cisco_switches.connect ()
+    print (cisco_switches.execute('show interface status'))
+    cisco_switches.disconnect ()
