2b1644807f3e4b2586889eaefe55614e75e6c546
mdziaurkhan <mdziaurkhan@gmail.com>
2021-06-02 02:49:20 -0500

    without mentiononiting any device

--- pyATS/lab1-vlan_creation/lab1_vlan_creattion.py
+++ pyATS/lab1-vlan_creation/lab1_vlan_creattion.py
@@ -3,7 +3,7 @@ from pyats.topology import loader
 testbed = loader.load('lab1_testbed_con.yaml')
 
 testbed.devices
-cisco_switches = testbed.devices[0]
+cisco_switches = testbed.devices
 
 cisco_switches.connect ()
 
