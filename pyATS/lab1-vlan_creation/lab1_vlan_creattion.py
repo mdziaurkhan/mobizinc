607c7e246ac4e41014ac8c0c4e775f269c6db286
mdziaurkhan <mdziaurkhan@gmail.com>
2021-06-02 03:11:39 -0500

    added a descryption

--- pyATS/lab1-vlan_creation/lab1_vlan_creattion.py
+++ pyATS/lab1-vlan_creation/lab1_vlan_creattion.py
@@ -4,7 +4,7 @@ testbed = loader.load('lab1_testbed_con.yaml')
 
 testbed.devices
 #cisco_switches = testbed.devices['NexSW-192.168.1.12']
-for i in testbed.devices:
+for i in testbed.devices:         # i is a variable.
     cisco_switches=testbed.devices[i]
     cisco_switches.connect ()
     print (cisco_switches.execute('show version'))
