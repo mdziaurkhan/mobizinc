ce50cee650395fcfcd17a8e2c16d0919e04a2cda
mdziaurkhan <mdziaurkhan@gmail.com>
2021-06-03 03:37:23 -0500

    cleaned up lines.

--- pyATS/lab2-connectivity_check/lab2-connectivity_check.py
+++ pyATS/lab2-connectivity_check/lab2-connectivity_check.py
@@ -1,25 +1,18 @@
 #   connectivity_check.py
 
 from pyats import aetest
-#from pyats.topology import loader
-
 
 class CommonSetup(aetest.CommonSetup):
     @aetest.subsection
     def establish_connections(self, steps):
-        #testbed = loader.load('lab2_testbed.yaml')
-        #testbed.devices
         for i in testbed.devices:
             with steps.start('Connecting to %s' % i):
                 testbed.devices[i].connect()
-
 
 class CommonCleanup(aetest.CommonCleanup):
 
     @aetest.subsection
     def disconnect(self, steps):
-        #testbed = loader.load('lab2_testbed.yaml')
-        #testbed.devices
         for i in testbed.devices:
             with steps.start('Disconnecting from %s' % i):
                 testbed.devices[i].disconnect()
@@ -31,12 +24,6 @@ if __name__ == '__main__':
     from pyats.topology import loader
 
     testbed = loader.load('lab2_testbed.yaml')
-
     testbed.devices
-
-    #for i in testbed.devices:
-    #parser = argparse.ArgumentParser()
-    #parser.add_argument('--testbed', dest = 'testbed',type = loader.load)
-    #args, unknown = parser.parse_known_args()
 
     aetest.main()
