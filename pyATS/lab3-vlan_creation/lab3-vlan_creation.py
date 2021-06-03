86cf835e48d5f3af3bdf28416198c80c6a286527
mdziaurkhan <mdziaurkhan@gmail.com>
2021-06-03 03:17:43 -0500

    trying to create vlan

--- pyATS/lab3-vlan_creation/lab3-vlan_creation.py
+++ pyATS/lab3-vlan_creation/lab3-vlan_creation.py
@@ -15,14 +15,15 @@ class CommonSetup(aetest.CommonSetup):
 
 class VlanCreation(aetest.Testcase):
     @aetest.setup
-    def Create_Vlan(self, steps):
+    def Create_Vlan(self, steps, vlan):
         testbed = loader.load('lab3_testbed.yaml')
         testbed.devices
         for i in testbed.devices:
             with steps.start('Configuring VLAN into %s' % i):
-                new_vlan = Vlan(vlan_id = "20", name = "ziatest")
-                testbed.devices[i].add_feature(new_vlan)
-                new_vlan.build_config(apply = True)
+                #new_vlan = Vlan(vlan_id = "20", name = "ziatest")
+                #testbed.devices[i].add_feature(new_vlan)
+                #new_vlan.build_config(apply = True)
+                logger.info('Vlan = %s' % vlan)
 
 class CommonCleanup(aetest.CommonCleanup):
 
@@ -40,12 +41,16 @@ if __name__ == '__main__':
     import argparse
     from pyats.topology import loader
     from genie.libs.conf.vlan import vlan
+    import sys
 
     testbed = loader.load('lab3_testbed.yaml')
     testbed.devices
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--testbed', dest = 'testbed',type = loader.load)
+    parser.add_argument('--vlan', dest = 'vlan')
     args, unknown = parser.parse_known_args()
 
+    vlan = int(args.vlan)
+    
     aetest.main(**vars(args))
