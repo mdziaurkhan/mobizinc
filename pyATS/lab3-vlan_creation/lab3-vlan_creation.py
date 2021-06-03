25606a14f9eb3197f35b706d58450c198206e337
mdziaurkhan <mdziaurkhan@gmail.com>
2021-06-03 01:39:52 -0500

    rearanging folder.

---
+++ pyATS/lab3-vlan_creation/lab3-vlan_creation.py
@@ -0,0 +1,42 @@
+#   connectivity_check.py
+
+from pyats import aetest
+from pyats.topology import loader
+
+
+class CommonSetup(aetest.CommonSetup):
+    @aetest.subsection
+    def establish_connections(self, steps):
+        testbed = loader.load('lab2_testbed.yaml')
+        testbed.devices
+        for i in testbed.devices:
+            with steps.start('Connecting to %s' % i):
+                testbed.devices[i].connect()
+
+
+class CommonCleanup(aetest.CommonCleanup):
+
+    @aetest.subsection
+    def disconnect(self, steps):
+        testbed = loader.load('lab2_testbed.yaml')
+        testbed.devices
+        for i in testbed.devices:
+            with steps.start('Disconnecting from %s' % i):
+                testbed.devices[i].disconnect()
+
+
+
+if __name__ == '__main__':
+    import argparse
+    from pyats.topology import loader
+
+    testbed = loader.load('lab2_testbed.yaml')
+
+    testbed.devices
+
+    #for i in testbed.devices:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--testbed', dest = 'testbed',type = loader.load)
+    args, unknown = parser.parse_known_args()
+
+    aetest.main(**vars(args))
