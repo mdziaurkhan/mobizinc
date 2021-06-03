2a1a898c37797630b6a5116b658dd11df6a8f337
mdziaurkhan <mdziaurkhan@gmail.com>
2021-06-02 23:42:48 -0500

    craating connect disconnect device.

---
+++ pyATS/lab2-connectivity_check/lab2-connectivity_check.py
@@ -0,0 +1,37 @@
+#   connectivity_check.py
+
+from pyats import aetest
+
+class CommonSetup(aetest.CommonSetup):
+
+    @aetest.subsection
+    def establish_connections(self, steps, i):
+        with steps.start('Connecting to %s' % i):
+            i.connect()
+
+
+class CommonCleanup(aetest.CommonCleanup):
+
+    @aetest.subsection
+    def disconnect(self, steps, i):
+        with steps.start('Disconnecting from %s' % i):
+            ios1.disconnect()
+
+
+if __name__ == '__main__':
+    import argparse
+    from pyats.topology import loader
+
+    testbed = loader.load('lab2_testbed.yaml')
+
+    testbed.devices
+    
+    for i in testbed.devices:         # i is a variable.
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--testbed', dest = 'testbed',
+                        type = loader.load)
+
+    args, unknown = parser.parse_known_args()
+
+    aetest.main(**vars(args))
