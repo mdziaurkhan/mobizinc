fefb1d81ae369748858157c53d159862da15e474
mdziaurkhan <mdziaurkhan@gmail.com>
2021-06-04 01:08:14 -0500

    checkin

--- pyATS/lab3-vlan_creation/lab3-vlan_creation-gen.py
+++ pyATS/lab3-vlan_creation/lab3-vlan_creation-gen.py
@@ -20,10 +20,10 @@ class VlanCreation(aetest.Testcase):
                 new_vlan = Vlan(vlan_id = "60", name = "ziatestGen")
                 testbed.devices[i].add_feature(new_vlan)
                 #new_vlan.build_config()
-                new_vlan.build_config(apply = True)[testbed.devices[i]]
+                output = new_vlan.build_config(apply = True)
                 #testbed.devices[i].build_config(new_vlan)
                 #testbed.devices[i].build_config(new_vlan)
-                #output(testbed.devices[i])
+                output[testbed.devices[i]]
 
 
 class CommonCleanup(aetest.CommonCleanup):
