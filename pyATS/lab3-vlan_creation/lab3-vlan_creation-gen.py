4677949de569ce6ac445d563d87d11b7e1b5ed01
mdziaurkhan <mdziaurkhan@gmail.com>
2021-06-03 23:43:30 -0500

    trying genie conf

---
+++ pyATS/lab3-vlan_creation/lab3-vlan_creation-gen.py
@@ -0,0 +1,48 @@
+#   connectivity_check.py
+
+from pyats import aetest
+
+class CommonSetup(aetest.CommonSetup):
+    @aetest.subsection
+    def establish_connections(self, steps):
+        for i in testbed.devices:
+            with steps.start('Connecting to %s' % i):
+                testbed.devices[i].connect()
+
+class VlanCreation(aetest.Testcase):
+    @aetest.setup
+    def Create_Vlan(self, steps):
+        for i in testbed.devices:
+            with steps.start('Configuring VLAN into %s' % i):
+                #testbed.devices[i].configure(""" vlan 20
+                name ziatest
+                """)
+                #testbed.devices[i].add_feature(new_vlan)
+                #new_vlan.build_config()
+                testbed.devices[i].build_config(new_vlan)
+
+
+class CommonCleanup(aetest.CommonCleanup):
+    @aetest.subsection
+    def disconnect(self, steps):
+        for i in testbed.devices:
+            with steps.start('Disconnecting from %s' % i):
+                testbed.devices[i].disconnect()
+
+
+
+if __name__ == '__main__':
+    import argparse
+    from pyats.topology import loader
+    from genie.libs.conf import vlan
+    #import genie
+
+
+
+    testbed = loader.load('lab3_testbed.yaml')
+    testbed.devices
+    new_vlan = vlan(vlan_id = "30", name = "ziatest")
+    #new_vlan.vlan_id = "20"
+    #new_vlan.name = "ziatest"
+
+    aetest.main()
